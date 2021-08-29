9cd1664e668a6e52f486bcdae61a1b72f512f012
imadhyan <adhyanchawla500@gmail.com>
2021-08-29 19:12:09 +0530

    commit

--- dp2.java
+++ dp2.java
@@ -23,13 +23,14 @@ public class dp2 {
     }
 
     public static void dpSolutions() {
-        int[][] board = new int[3][3];
-        int[][]dp = new int[3][3];
-        int sr = 0, sc = 0, er = 2, ec = 2;
+        //int[][]dp = new int[4][4];
+        int sr = 0, sc = 0, er = 3, ec = 3;
 
         int[][] dir = {{0, 1}, {1, 0}, {1, 1}};
-        System.out.println(mazePathWithMultipleJumps(sr, sc, er, ec, dp, dir));
-        display2D(dp);
+        int[] dp = new int[11];
+        //System.out.println(mazePathWithMulipleJumps_mem(sr, sc, er, ec, dp, dir));
+        System.out.println(boardPath_tab(0, 10, dp));
+        display(dp);
     }
 
     //we need to store the count in the cells we covered to calc the paths to reach to that cell so we need 2D dp
@@ -87,6 +88,26 @@ public class dp2 {
         return dp[SR][SC];
     }
 
+    public static int mazePathWithMulipleJumps_mem(int sr, int sc, int er, int ec, int[][] dp, int[][] dir ) {
+        if(sr == er && sc == ec) {
+            return dp[sr][sc] = 1;
+        }
+
+        int count = 0;
+
+        for(int d = 0; d < dir.length; d++) {
+            int r = sr + dir[d][0], c = sc + dir[d][1];
+
+            while(r >= 0 && c >= 0 && r < dp.length && c < dp[0].length) {
+                count += mazePathWithMulipleJumps_mem(r, c, er, ec, dp, dir);
+                r += dir[d][0];
+                c += dir[d][1];
+            }
+        }
+
+        return dp[sr][sc] = count;
+    }
+
     public static int mazePathWithMultipleJumps(int SR, int SC, int ER, int EC, int[][] dp, int[][] dir) {
         for(int sr = ER; sr >= SR; sr--) {
             for(int sc = EC; sc >= SC; sc--) {
@@ -114,5 +135,43 @@ public class dp2 {
         return dp[SR][SC];
     }
 
-    
+    //===========================================================================
+
+    //boardpath
+
+    //using dice
+    public static int boardPath(int sr, int er, int[] dp) {
+        if(sr == er) {
+            return dp[sr] = 1;
+        }
+
+        if(dp[sr] != 0) return dp[sr];
+
+        int count = 0;
+
+        for(int dice = 1; dice <= 6 && dice + sr <= er; dice++) {
+            count += boardPath(sr + dice, er, dp);
+        }
+
+        return dp[sr] = count;
+    }
+
+    public static int boardPath_tab(int SR, int ER, int[] dp) {
+
+        for(int sr = ER; sr >= SR; sr--) {
+            if(sr == ER) {
+                dp[sr] = 1;
+                continue;
+            }
+
+            int count = 0;
+            for(int dice = 1; dice <= 6 && dice + sr <= ER; dice++) {
+                count += dp[sr + dice];
+            }
+
+            dp[sr] = count;
+        }
+
+        return dp[SR];
+    }    
 }
