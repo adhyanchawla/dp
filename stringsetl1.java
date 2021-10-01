7ca4a15ebfc65e9ad293cfad368565906194b109
imadhyan <adhyanchawla500@gmail.com>
2021-10-01 11:55:26 +0530

    commit

--- stringsetl1.java
+++ stringsetl1.java
@@ -88,9 +88,143 @@ public class stringsetl1 {
         return dp[I][J];
     }
 
-    public static void main(String[] args) {
-        longestPalindromicSubsequnce();
+    //====================================================================================================================
+    //longest palindrome substring
+    public static void longestPalindromicSubstring() {
+        String str = "lgeekfkeeg";
+        int n = str.length();
+        int[][] dp = new int[n][n];
+
+        System.out.println(lps(str, dp));
+        display2D(dp);
     }
 
+    public static String lps(String str, int[][] dp) {
+        int n = str.length();
+        int mi = 0;
+        int mj = 0;
+        int count = 0;
+        int max = 0;
+        for(int gap = 0; gap < n; gap++) {
+            for(int i = 0, j = gap; i < n && j < n; i++, j++) {
+                if(gap == 0) {
+                    dp[i][j] = 1;
+                } else if(gap == 1 && str.charAt(i) == str.charAt(j)) {
+                    dp[i][j] = 2;
+                } else if(str.charAt(i) == str.charAt(j) && dp[i + 1][j - 1] > 0) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                }
 
+                if(dp[i][j] != 0) {
+                    count++; //count of all the palindromic substrings
+                }
+
+                if(max < dp[i][j]) {
+                    max = dp[i][j]; //max length of the substring which is palindrome
+                    mi = i;
+                    mj = j;
+                }
+            }
+        }
+        return str.substring(mi, mj + 1); //returns the longest palindrome substring
+    }
+    //==========================================================================================================
+    //lcss
+    public static void longestCommonSubsequence() {
+        String s1 = "abcde";
+        String s2 = "ace";
+        int n = s1.length(), m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        // for(int[]d : dp) {
+        //     Arrays.fill(d, -1);
+        // }
+        System.out.println(lcss_dp(s1, s2, n, m, dp));
+        display2D(dp);
+
+    }
+
+    public static int lcss(String s1, String s2, int n, int m, int[][] dp) {
+        if(n == 0 || m == 0) {
+            return dp[n][m] = 0;
+        }
+
+        if(dp[n][m] != -1) {
+            return dp[n][m];
+        }
+
+        if(s1.charAt(n - 1) == s2.charAt(m - 1)) {
+            return dp[n][m] = lcss(s1, s2, n - 1, m - 1, dp) + 1;
+        } else {
+            return dp[n][m] = Math.max(lcss(s1, s2, n - 1, m, dp), lcss(s1, s2, n, m - 1, dp));
+        }
+    }
+
+    public static int lcss_dp(String s1, String s2, int N, int M, int[][] dp) {
+        for(int n = 0; n <= N; n++) {
+            for(int m = 0; m <= M; m++) {
+                if(n == 0 || m == 0) {
+                    dp[n][m] = 0;
+                    continue;
+                }
+        
+                if(s1.charAt(n - 1) == s2.charAt(m - 1)) {
+                    dp[n][m] = dp[n - 1][m - 1] + 1;
+                } else {
+                    dp[n][m] = Math.max(dp[n - 1][m], dp[n][m - 1]);
+                }
+            }
+        }
+        return dp[N][M];
+    }
+
+    //==============================================================================================
+    //lc 1035
+    public static int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+        for(int[]d : dp) {
+            Arrays.fill(d, -1);
+        }
+        return maxLines(nums1, nums2, nums1.length, nums2.length, dp);
+    }
+    
+    public static int maxLines(int[] arr1, int[] arr2, int i, int j, int[][] dp) {
+        if(i == 0 || j == 0) {
+            return dp[i][j] = 0;
+        }
+        
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        
+        if(arr1[i - 1] == arr2[j - 1]) {
+            return dp[i][j] = 1 + maxLines(arr1, arr2, i - 1, j - 1, dp);
+        } else {
+            return dp[i][j] = Math.max(maxLines(arr1, arr2, i, j - 1, dp), maxLines(arr1, arr2, i - 1, j, dp));
+        }
+    }
+
+    public static int maxLines_dp(int[] arr1, int[] arr2, int N, int M, int[][] dp) {
+        for(int n = 0; n <= N; n++) {
+            for(int m = 0; m <= M; m++) {
+                    if(n == 0 || m == 0) {
+                        dp[n][m] = 0;
+                        continue;
+                    }
+
+            if(arr1[n - 1] == arr2[m - 1]) {
+                dp[n][m] = 1 + dp[n - 1][m -1];
+            } else {
+                dp[n][m] = Math.max(dp[n][m -1], dp[n - 1][m]);
+                }
+            }
+        }
+    
+        return dp[N][M];
+    }
+
+    public static void main(String[] args) {
+        //longestPalindromicSubsequnce();
+        //longestPalindromicSubstring();
+        longestCommonSubsequence();
+    }
 }
