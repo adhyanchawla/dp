98a4a43e3aef316a69dfd827dbd8e55b3b9fa760
imadhyan <adhyanchawla500@gmail.com>
2021-10-25 08:54:13 +0530

    commit

--- targetSet.java
+++ targetSet.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 public class targetSet {
 
     public static void display(int[] dp) {
@@ -36,11 +35,8 @@ public class targetSet {
     }
 
     public static int permutation_tab(int[] arr, int Tar, int[] dp) {
+        dp[0] = 1;
         for(int tar = 0; tar <= Tar; tar++) {
-            if(tar == 0) {
-                dp[tar] = 1;
-                continue;
-            }
             int count = 0;
             for(int i = 0; i < arr.length; i++) {
                 if(tar - arr[i] >= 0) {
@@ -54,14 +50,53 @@ public class targetSet {
         return dp[Tar];
     }
 
+    public static int combination_memo(int[] arr, int n, int tar, int[][] dp) {
+        if(tar == 0) {
+            return dp[n][tar] = 1;
+        }
+
+        if(dp[n][tar] != -1) {
+            return dp[n][tar];
+        }
+
+        int count = 0;
+        for(int i = n - 1; i >= 0; i--) {
+            if(tar - arr[i] >= 0)
+            count += combination_memo(arr, i + 1, tar - arr[i], dp);
+        }
+
+        return dp[n][tar] = count;
+    }
+
+    public static int combination_tab(int[] arr, int Tar, int[] dp) {
+        dp[0] = 1;
+        for(int ele : arr) {
+            for(int tar = 0; tar <= Tar; tar++) {
+
+                if(tar - ele >= 0) {
+                    dp[tar] += dp[tar - ele];
+                }
+            }
+        }
+
+        return dp[Tar];
+    }
+
     public static void coinSet() {
         int[] coins = {2, 3, 5, 7};
         int tar = 10;
+
+        // int[][] dp = new int[coins.length + 1][tar + 1];
+        // for(int[] d : dp) Arrays.fill(d, - 1);
+        // System.out.println(combination_memo(coins, coins.length, tar, dp));
+        // display2D(dp);
         int[] dp = new int[tar + 1];
+        System.out.println(combination_tab(coins, tar, dp));
+        display(dp);
         //Arrays.fill(dp, -1);
         //System.out.println(permutation_memo(coins, tar, dp));
-        System.out.println(permutation_tab(coins, tar, dp));
-        display(dp);
+        //System.out.println(permutation_tab(coins, tar, dp));
+        //display(dp);
     }
 
     public static void main(String[] args) {
