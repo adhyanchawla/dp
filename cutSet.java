6608d6a670ae29a71d83f9343ce7976b27b95c3c
imadhyan <adhyanchawla500@gmail.com>
2021-10-25 23:52:24 +0530

    commit

--- cutSet.java
+++ cutSet.java
@@ -42,15 +42,89 @@ public class cutSet {
         return dp[si][ei] = minRes;
     }
 
+    public static int mcm_tabu(int[] arr, int SI, int EI, int[][] dp) {
+        int n = arr.length;
+        for(int gap = 1; gap < n; gap++) {
+            for(int si = 0, ei = gap; si < n && ei < n; si++, ei++) {
+                if(si + 1 == ei) {
+                    dp[si][ei] = 0;
+                    continue;
+                }
+        
+                int minRes = (int)1e8;
+                for(int cut = si + 1; cut < ei; cut++) {
+                    int leftRes = dp[si][cut];//mcm_memo(arr, si, cut, dp);
+                    int rightRes = dp[cut][ei];//mcm_memo(arr, cut, ei, dp);
+        
+                    int res = leftRes + arr[cut] * arr[si] * arr[ei] + rightRes;
+        
+                    if(res < minRes) {
+                        minRes = res;
+                    }
+                }
+        
+                dp[si][ei] = minRes;
+            }
+        }
+
+        return dp[SI][EI];
+    }
+
+    //=================================================================================================
+    // https://practice.geeksforgeeks.org/problems/brackets-in-matrix-chain-multiplication1024/1#
+    //matrix chain order
+    public static String matrixChainOrder(int p[], int n){
+        // code here
+        String[][] sdp = new String[n][n];
+        int[][] dp = new int[n][n];
+        
+        for(String[] s : sdp) {
+            Arrays.fill(s, "");
+        }
+        
+        int SI = 0;
+        int EI = n - 1;
+        
+        for(int gap = 1; gap < n; gap++) {
+            for(int si = 0, ei = gap; si < n && ei < n; si++, ei++) {
+                if(si + 1 == ei) {
+                    dp[si][ei] = 0;
+                    sdp[si][ei] = (char)(si + 'A') + "";
+                    continue;
+                }
+                
+                int minRes = (int)1e9;
+                String minStr = "";
+                for(int cut = si + 1; cut < ei; cut++) {
+                    int leftRes = dp[si][cut];
+                    int rightRes = dp[cut][ei];
+                    
+                    int res = leftRes + p[si] * p[ei] * p[cut] + rightRes;
+                    
+                    if(res < minRes) {
+                        minRes = res;
+                        minStr = "(" + sdp[si][cut] + sdp[cut][ei] + ")";
+                    }
+                }
+                
+                dp[si][ei] = minRes;
+                sdp[si][ei] = minStr;
+            }
+        }
+        
+        return sdp[SI][EI];
+    }
+
     public static void cutSet01() {
         int[] arr = {4, 2, 3, 2, 3, 5, 4};
         int n = arr.length;
         int[][] dp = new int[n][n];
-        for(int []d : dp) {
-            Arrays.fill(d, -1);
-        }
+        // for(int []d : dp) {
+        //     Arrays.fill(d, -1);
+        // }
 
-        System.out.println(mcm_memo(arr, 0, arr.length - 1, dp));
+        //System.out.println(mcm_memo(arr, 0, arr.length - 1, dp));
+        System.out.println(mcm_tabu(arr, 0, n - 1, dp));
         display2D(dp);
     }
     public static void main(String[] args) {
